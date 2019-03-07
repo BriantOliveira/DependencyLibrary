27aa3f8a8ee8a201ed78fa71f9c35f341834e542
Elliot Briant <bmdeoliv@jssla17101376.ndc.nasa.gov>
2019-03-07 15:49:26 -0600

    Adding user and repository structs

---
+++ user.go
@@ -0,0 +1,71 @@
+package library
+
+import "time"
+
+type User struct {
+	ID 				*int		`json:"id, omitempty"`
+	UUID 			*int		`json:"uuid, omitempty"`
+	Login			*string		`json:"login, omitempty"`
+	UserType		*string		`json:"user_type, omitempty"`
+	CreatedAt		*time.Time	`json:"created_at, omitempty"`
+	UpdatedAt		*time.Time	`json:"updated_at, omitempty"`
+	Name 			*string 	`json:"name, omitempty"`
+	Company			*string		`json:"company, omitempty"`
+	Blog			*string		`json:"blog, omitempty"`
+	Location 		*string 	`json:"location, omitempty"`
+	Hidden 			*bool		`json:"hidden, omitempty"`
+	LastSyncedAt 	*time.Time	`json:"last_synced, omitempty"`
+	Email			*string		`json:"email, omitempty"`
+	Bio 			*string 	`json:"bio, omitempty"`
+	Followers		*int		`json:"followers, omitempty"`
+	Following		*int		`json:"following, omitempty"`
+	HostType		*string		`json:"host_type, omitempty"`
+	Github			*int		`json:"hithub_id, ommitempty"`
+}
+
+type Repository struct {
+	ContributionsCount       *int       `json:"contributions_count,omitempty"`
+	CreatedAt                *time.Time `json:"created_at,omitempty"`
+	DefaultBranch            *string    `json:"default_branch,omitempty"`
+	Description              *string    `json:"description,omitempty"`
+	Fork                     *bool      `json:"fork,omitempty"`
+	ForkPolicy               *string    `json:"fork_policy,omitempty"`
+	ForksCount               *int       `json:"forks_count,omitempty"`
+	FullName                 *string    `json:"full_name,omitempty"`
+	GithubContributionsCount *int       `json:"github_contributions_count,omitempty"`
+	GithubID                 *string    `json:"github_id,omitempty"`
+	HasAudit                 *string    `json:"has_audit,omitempty"`
+	HasChangelog             *string    `json:"has_changelog,omitempty"`
+	HasCoc                   *string    `json:"has_coc,omitempty"`
+	HasContributing          *string    `json:"has_contributing,omitempty"`
+	HasIssues                *bool      `json:"has_issues,omitempty"`
+	HasLicense               *string    `json:"has_license,omitempty"`
+	HasPages                 *bool      `json:"has_pages,omitempty"`
+	HasReadme                *string    `json:"has_readme,omitempty"`
+	HasThreatModel           *string    `json:"has_threat_model,omitempty"`
+	HasWiki                  *bool      `json:"has_wiki,omitempty"`
+	Homepage                 *string    `json:"homepage,omitempty"`
+	HostDomain               *string    `json:"host_domain,omitempty"`
+	HostType                 *string    `json:"host_type,omitempty"`
+	Keywords                 []*string  `json:"keywords,omitempty"`
+	Language                 *string    `json:"language,omitempty"`
+	LastSyncedAt             *time.Time `json:"last_synced_at,omitempty"`
+	License                  *string    `json:"license,omitempty"`
+	LogoURL                  *string    `json:"logo_url,omitempty"`
+	MirrorURL                *string    `json:"mirror_url,omitempty"`
+	Name                     *string    `json:"name,omitempty"`
+	OpenIssuesCount          *int       `json:"open_issues_count,omitempty"`
+	Private                  *bool      `json:"private,omitempty"`
+	PullRequestsEnabled      *bool      `json:"pull_requests_enabled,omitempty"`
+	PushedAt                 *time.Time `json:"pushed_at,omitempty"`
+	Rank                     *int       `json:"rank,omitempty"`
+	Scm                      *string    `json:"scm,omitempty"`
+	Size                     *int       `json:"size,omitempty"`
+	SourceName               *string    `json:"source_name,omitempty"`
+	StargazersCount          *int       `json:"stargazers_count,omitempty"`
+	Status                   *string    `json:"status,omitempty"`
+	SubscribersCount         *int       `json:"subscribers_count,omitempty"`
+	UUID                     *string    `json:"uuid,omitempty"`
+	UpdatedAt                *time.Time `json:"updated_at,omitempty"`
+}
+

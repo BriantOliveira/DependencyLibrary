dbd2539e6d1dbee22597170bd8ed8d6fad3be6c5
Elliot Briant <bmdeoliv@jssla17101376.ndc.nasa.gov>
2019-03-07 13:28:19 -0600

    Created project struct

--- projects.go
+++ projects.go
@@ -1,6 +1,29 @@
 package library
 
+import "time"
+
 // Project represents the table of project in libraries.io
 type Project struct {
-	Description 	*string		`json:"description, omitempty"`
+	Description              *string    `json:"description,omitempty"`
+	Forks                    *int       `json:"forks,omitempty"`
+	Homepage                 *string    `json:"homepage,omitempty"`
+	Keywords                 []*string  `json:"keywords,omitempty"`
+	Language                 *string    `json:"language,omitempty"`
+	LatestReleaseNumber      *string    `json:"latest_release_number,omitempty"`
+	LatestReleasePublishedAt *time.Time `json:"latest_release_published_at,omitempty"`
+	LatestStableRelease      *Releases   `json:"latest_stable_release,omitempty"`
+	Name                     *string    `json:"name,omitempty"`
+	NormalizedLicenses       []*string  `json:"normalized_licenses,omitempty"`
+	PackageManagerURL        *string    `json:"package_manager_url,omitempty"`
+	Platform                 *string    `json:"platform,omitempty"`
+	Rank                     *int       `json:"rank,omitempty"`
+	Stars                    *int       `json:"stars,omitempty"`
+	Status                   *string    `json:"status,omitempty"`
+	Versions                 []*Releases`json:"versions,omitempty"`
+
+	// Dependencies are only populated for Project Dependencies
+	Dependencies []*ProjectDependency `json:"dependencies,omitempty"`
+
+	// RepositoryURL is only populated for User Projects
+	RepositoryURL *string `json:"repository_url,omitempty"`
 }
